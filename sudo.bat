81201359a547cdf9fdbdfb533200e88ad41d2ee3
Jesterhearts <Jesterhearts@users.noreply.github.com>
2015-12-22 19:04:10 -0800

    Make more things work with sudo
    The previous version had some weird quote corner cases it couldn't handle.

--- sudo.bat
+++ sudo.bat
@@ -1,15 +1,14 @@
 :: ------- Self-elevating.bat --------------------------------------
-:: Modified from http://stackoverflow.com/questions/5944180/how-do-you-run-a-command-as-an-administrator-from-the-windows-shell
+:: See http://stackoverflow.com/questions/5944180/how-do-you-run-a-command-as-an-administrator-from-the-windows-shell
 
 @echo off
-:: This juggles the parameters around so that any input parameters can be quoted
-:: regardless of whether or not they start out quoted
-set var=%*
-set "var=%var:"=%"
+:: This uses the magic of however cmd /c parses quotes to let pretty much any combination work
+:: It even allows for stuff like sudo "del "C:\Path with spaces\temp.txt""
+echo cmd /c %* > %LOCALAPPDATA%\sudo_command.bat
 findstr "^:::" "%~sf0"> %LOCALAPPDATA%\sudo_temp.vbs
-cscript //nologo %LOCALAPPDATA%\sudo_temp.vbs %CD% "%var%" & exit /b
+cscript //nologo %LOCALAPPDATA%\sudo_temp.vbs %CD% & del %LOCALAPPDATA%\sudo_temp.vbs & exit /b
 
 ::: Set objShell = CreateObject("Shell.Application")
 ::: Set objWshShell = WScript.CreateObject("WScript.Shell")
 ::: Set objWshProcessEnv = objWshShell.Environment("PROCESS")
-::: objShell.ShellExecute "cmd", "/c cd " & WScript.Arguments.Item(0) & " & echo " & WScript.Arguments.Item(0) & "^>Running as Admin: """ & WScript.Arguments.Item(1) & """ & cmd /c " & WScript.Arguments.Item(1) & " & pause", "", "runas"
+::: objShell.ShellExecute "cmd", "/c cd " & WScript.Arguments.Item(0) & " & %LOCALAPPDATA%\sudo_command.bat & pause & del %LOCALAPPDATA%\sudo_command.bat", "", "runas"
