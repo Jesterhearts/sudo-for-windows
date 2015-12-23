db4dce1f34c4957cb163043733c71bf6d81f4e11
Jesterhearts <j.tl.summers@gmail.com>
2015-12-22 21:35:01 -0800

    Making the tempfiles use uuids

--- install_sudo.bat
+++ install_sudo.bat
@@ -1,5 +1,9 @@
 @echo off
 :: Assuming this file is in the same directory as sudo.bat
+:: Create the uuid generator
+echo set obj = CreateObject("Scriptlet.TypeLib") > %LOCALAPPDATA%\sudo_gen_uuid.vbs
+echo WScript.StdOut.WriteLine Replace(Replace(Replace(obj.GUID,"{",""),"}",""),"-","_") >> %LOCALAPPDATA%\sudo_gen_uuid.vbs
+:: Copy the bat file to appdata
 copy sudo.bat %LOCALAPPDATA%\sudo.bat
 :: Write the regkey for aliasing sudo
 ::                                                                                                    --       Escape hell      --
--- sudo.bat
+++ sudo.bat
@@ -2,13 +2,15 @@
 :: See http://stackoverflow.com/questions/5944180/how-do-you-run-a-command-as-an-administrator-from-the-windows-shell
 
 @echo off
+:: Generate a uuid to user for the command and launcher script
+for /f "delims=" %%i in ('cscript //nologo %LOCALAPPDATA%\sudo_gen_uuid.vbs') do set filename=%%i
 :: This uses the magic of however cmd /c parses quotes to let pretty much any combination work
 :: It even allows for stuff like sudo "del "C:\Path with spaces\temp.txt""
-echo cmd /c %* > %LOCALAPPDATA%\sudo_command.bat
-findstr "^:::" "%~sf0"> %LOCALAPPDATA%\sudo_temp.vbs
-cscript //nologo %LOCALAPPDATA%\sudo_temp.vbs %CD% & del %LOCALAPPDATA%\sudo_temp.vbs & exit /b
+echo cmd /c %* > %LOCALAPPDATA%\%filename%.bat
+findstr "^:::" "%~sf0"> %LOCALAPPDATA%\%filename%.vbs
+cscript //nologo %LOCALAPPDATA%\%filename%.vbs %CD% %filename% & del %LOCALAPPDATA%\%filename%.vbs & exit /b
 
 ::: Set objShell = CreateObject("Shell.Application")
 ::: Set objWshShell = WScript.CreateObject("WScript.Shell")
 ::: Set objWshProcessEnv = objWshShell.Environment("PROCESS")
-::: objShell.ShellExecute "cmd", "/c cd " & WScript.Arguments.Item(0) & " & %LOCALAPPDATA%\sudo_command.bat & pause & del %LOCALAPPDATA%\sudo_command.bat", "", "runas"
+::: objShell.ShellExecute "cmd", "/c cd " & WScript.Arguments.Item(0) & " & %LOCALAPPDATA%\" & WScript.Arguments.Item(1)  & ".bat & pause & del %LOCALAPPDATA%\" & WScript.Arguments.Item(1) & ".bat", "", "runas"
